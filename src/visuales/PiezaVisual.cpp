4350320a95bbaacdc1c69eb9ca2fb438688cf6c4
AndresCortesVictor <andres.cortes.victor@est.una.ac.cr>
2026-09-14 00:09:06 -0600

    Visuales de la Pieza

--- src/visuales/PiezaVisual.cpp
+++ src/visuales/PiezaVisual.cpp
@@ -8,21 +8,70 @@ PiezaVisual::PiezaVisual() {
     }
 }
 
-void PiezaVisual::configurarPiezaPrueba(float xOrigenTablero, float yOrigenTablero, sf::Color color) {
-    int coordenadas[4][2] = {
-        {4, 0},
-        {3, 1},
-        {4, 1},
-        {5, 1}
-    };
-    
+void PiezaVisual::configurar(TipoPieza tipo, int gridX, int gridY) {
+    int coordenadas[4][2] = {0};
+    sf::Color color;
+
+    switch (tipo) {
+        case TipoPieza::I:
+            coordenadas[0][0] = 0; coordenadas[0][1] = 1;
+            coordenadas[1][0] = 1; coordenadas[1][1] = 1;
+            coordenadas[2][0] = 2; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 3; coordenadas[3][1] = 1;
+            color = sf::Color::Cyan;
+            break;
+        case TipoPieza::O:
+            coordenadas[0][0] = 1; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 2; coordenadas[1][1] = 0;
+            coordenadas[2][0] = 1; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 2; coordenadas[3][1] = 1;
+            color = sf::Color::Yellow;
+            break;
+        case TipoPieza::T:
+            coordenadas[0][0] = 1; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 0; coordenadas[1][1] = 1;
+            coordenadas[2][0] = 1; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 2; coordenadas[3][1] = 1;
+            color = sf::Color::Magenta;
+            break;
+        case TipoPieza::S:
+            coordenadas[0][0] = 1; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 2; coordenadas[1][1] = 0;
+            coordenadas[2][0] = 0; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 1; coordenadas[3][1] = 1;
+            color = sf::Color::Green;
+            break;
+        case TipoPieza::Z:
+            coordenadas[0][0] = 0; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 1; coordenadas[1][1] = 0;
+            coordenadas[2][0] = 1; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 2; coordenadas[3][1] = 1;
+            color = sf::Color::Red;
+            break;
+        case TipoPieza::J:
+            coordenadas[0][0] = 0; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 0; coordenadas[1][1] = 1;
+            coordenadas[2][0] = 1; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 2; coordenadas[3][1] = 1;
+            color = sf::Color::Blue;
+            break;
+        case TipoPieza::L:
+            coordenadas[0][0] = 2; coordenadas[0][1] = 0;
+            coordenadas[1][0] = 0; coordenadas[1][1] = 1;
+            coordenadas[2][0] = 1; coordenadas[2][1] = 1;
+            coordenadas[3][0] = 2; coordenadas[3][1] = 1;
+            color = sf::Color(255, 165, 0);
+            break;
+        default:
+            color = sf::Color::Transparent;
+            break;
+    }
+
     for (int i = 0; i < 4; ++i) {
         bloques[i].setFillColor(color);
-        
-        float posX = xOrigenTablero + (coordenadas[i][0] * TAMANO_CELDA);
-        float posY = yOrigenTablero + (coordenadas[i][1] * TAMANO_CELDA);
-        
-        bloques[i].setPosition(sf::Vector2f(posX, posY));
+        float px = ORIGEN_X + ((gridX + coordenadas[i][0]) * TAMANO_CELDA);
+        float py = ORIGEN_Y + ((gridY + coordenadas[i][1]) * TAMANO_CELDA);
+        bloques[i].setPosition(sf::Vector2f(px, py));
     }
 }
 
--- src/visuales/PiezaVisual.h
+++ src/visuales/PiezaVisual.h
@@ -2,15 +2,18 @@
 #define PIEZA_VISUAL_H
 
 #include <SFML/Graphics.hpp>
+#include "../Game/TipoPieza.h"
 
 class PiezaVisual {
 private:
     sf::RectangleShape bloques[4];
-    const float TAMANO_CELDA = 30.0f;
+    static constexpr float TAMANO_CELDA = 30.0f;
+    static constexpr float ORIGEN_X = 250.0f;
+    static constexpr float ORIGEN_Y = 0.0f;
 
 public:
     PiezaVisual();
-    void configurarPiezaPrueba(float xOrigenTablero, float yOrigenTablero, sf::Color color);
+    void configurar(TipoPieza tipo, int gridX, int gridY);
     void renderizar(sf::RenderWindow& ventana);
 };
 
