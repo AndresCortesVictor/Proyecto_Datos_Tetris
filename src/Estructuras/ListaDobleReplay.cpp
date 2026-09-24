365a794d65fdd8c776df07b8dae9e9456c54d826
AndresCortesVictor <andres.cortes.victor@est.una.ac.cr>
2026-09-24 08:28:12 -0600

    Estructura para el sistema de repeticion

--- src/Estructuras/ListaDobleReplay.cpp
+++ src/Estructuras/ListaDobleReplay.cpp
@@ -1,7 +1,77 @@
 #include "ListaDobleReplay.h"
+#include <iostream>
 
-ListaDobleReplay::ListaDobleReplay() {
+ListaDobleReplay::ListaDobleReplay() : cabeza(nullptr), cola(nullptr), actual(nullptr) {
 }
 
 ListaDobleReplay::~ListaDobleReplay() {
+    limpiar();
+}
+
+void ListaDobleReplay::limpiar() {
+    NodoReplay* temp = cabeza;
+    while (temp != nullptr) {
+        NodoReplay* borrar = temp;
+        temp = temp->siguiente;
+        delete borrar;
+    }
+    cabeza = nullptr;
+    cola = nullptr;
+    actual = nullptr;
+}
+
+void ListaDobleReplay::registrarEstado(const ListaTablero& tableroActual, int puntajeActual) {
+    if (actual != nullptr && actual != cola) {
+        NodoReplay* temp = actual->siguiente;
+        while (temp != nullptr) {
+            NodoReplay* borrar = temp;
+            temp = temp->siguiente;
+            delete borrar;
+        }
+        cola = actual;
+        cola->siguiente = nullptr;
+    }
+
+    EstadoReplay nuevoEstado;
+    nuevoEstado.puntaje = puntajeActual;
+    for (int i = 0; i < 20; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            nuevoEstado.tablero[i][j] = tableroActual.obtenerCelda(i, j);
+        }
+    }
+
+    NodoReplay* nuevoNodo = new NodoReplay(nuevoEstado);
+    
+    if (cabeza == nullptr) {
+        cabeza = nuevoNodo;
+        cola = nuevoNodo;
+    } else {
+        cola->siguiente = nuevoNodo;
+        nuevoNodo->anterior = cola;
+        cola = nuevoNodo;
+    }
+    
+    actual = cola;
+}
+
+bool ListaDobleReplay::hayAnterior() const {
+    return actual != nullptr && actual->anterior != nullptr;
+}
+
+bool ListaDobleReplay::haySiguiente() const {
+    return actual != nullptr && actual->siguiente != nullptr;
+}
+
+EstadoReplay ListaDobleReplay::retroceder() {
+    if (hayAnterior()) {
+        actual = actual->anterior;
+    }
+    return actual->estado;
+}
+
+EstadoReplay ListaDobleReplay::avanzar() {
+    if (haySiguiente()) {
+        actual = actual->siguiente;
+    }
+    return actual->estado;
 }
--- src/Estructuras/ListaDobleReplay.h
+++ src/Estructuras/ListaDobleReplay.h
@@ -1,10 +1,41 @@
 #ifndef LISTA_DOBLE_REPLAY_H
 #define LISTA_DOBLE_REPLAY_H
 
+#include "../Game/TipoPieza.h"
+#include "../Estructuras/ListaTablero.h"
+
+struct EstadoReplay {
+    TipoPieza tablero[20][10];
+    int puntaje;
+};
+
+struct NodoReplay {
+    EstadoReplay estado;
+    NodoReplay* siguiente;
+    NodoReplay* anterior;
+
+    NodoReplay(const EstadoReplay& est) : estado(est), siguiente(nullptr), anterior(nullptr) {}
+};
+
 class ListaDobleReplay {
+private:
+    NodoReplay* cabeza;
+    NodoReplay* cola;
+    NodoReplay* actual;
+
 public:
     ListaDobleReplay();
     ~ListaDobleReplay();
+
+    void registrarEstado(const ListaTablero& tablero, int puntaje);
+    
+    bool hayAnterior() const;
+    bool haySiguiente() const;
+    
+    EstadoReplay retroceder();
+    EstadoReplay avanzar();
+    
+    void limpiar();
 };
 
 #endif
